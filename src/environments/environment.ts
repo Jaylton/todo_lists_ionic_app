c5454b6df412910c46e0aa9ce2fc6a1f92c76be7
Jaylton Alencar <jayltonokra15@gmail.com>
2024-10-11 11:10:31 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyBWDlETrUnRcRyM4jzg78-upG-6dg7qK6c",
-    authDomain: "curso-ionic-564de.firebaseapp.com",
-    databaseURL: "https://curso-ionic-564de.firebaseio.com",
-    projectId: "curso-ionic-564de",
-    storageBucket: "curso-ionic-564de.appspot.com",
-    messagingSenderId: "1016015645984",
-    appId: "1:1016015645984:web:6db251a083c74c6f1ebb45"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
